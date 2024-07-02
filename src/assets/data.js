2d2c9c50f431f887b761ce786be8c1b3df7e1fb9
navyakaushik <navyakaushik678@gmail.com>
2024-07-02 14:07:06 +0530

    completed

---
+++ src/assets/data.js
@@ -0,0 +1,51 @@
+// data.js
+
+const quizData = [
+    {
+      id: 1,
+      question: "Which programming language is known as the 'mother of all languages'?",
+      option1: "Java",
+      option2: "C",
+      option3: "Python",
+      option4:"Assembly",
+      ans: 2,
+    },
+    {
+      id: 2,
+      question: "What does HTML stand for?",
+      option1: "Hyper Text Markup Language",
+      option2: "High Tech Multi-Language",
+      option3: "Hyper Transfer Markup Language",
+      option4: "Home Tool Markup Language",
+      ans: 1,
+    },
+    {
+      id: 3,
+      question: "Which of the following is NOT a JavaScript framework or library?",
+      option1: "React",
+      option2: "Angular",
+      option3: "Vue",
+      option4: "Java Spring",
+      ans: 4,
+    },
+    {
+      id: 4,
+      question: "What is the purpose of CSS in web development?",
+      option1: "To define the structure of web pages",
+      option2: "To style and layout web pages",
+      option3: "To add interactivity to web pages",
+      option4: "To handle server-side logic",
+      ans: 2,
+    },
+    {
+      id: 5,
+      question: "Which of these is a version control system?",
+      option1: "Docker",
+      option2: "Jenkins",
+      option3: "Git",
+      option4:"Kubernetes",
+      ans: 3,
+    }
+  ];
+  
+  export default quizData;
